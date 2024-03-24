c82dd18c5c24d65e2f68b4cfc3bc29384504ec7c
Khalid Zubair <khalid.zubair@gig.com>
2024-03-24 03:24:52 +0100

    Add flake.nix file for Nix Flakes/Home Manager

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Simple Nix Flakes/Home Manager";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  }; 
+
+  outputs = { self, nixpkgs, ... }:
+    let 
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations {
+        nixos = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+            ./system/flake_settings.nix
+            ./system/one_password.nix
+            ./system/users.nix
+          ];
+        };
+      };
+    };
+}
