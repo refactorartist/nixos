0f715300993467934bd1d041c2837a660ba3e940
Khalid Zubair <khalid.zubair@gig.com>
2024-03-24 03:04:35 +0100

    Add users.nix file with zsh enabled and user 'khalid' configured

---
+++ system/users.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+{
+  programs.zsh.enable = true;
+  users.users.khalid = {
+    isNormalUser = true;
+    description = "khalid";
+    extraGroups = [ 
+      "networkmanager" 
+      "wheel" 
+      "docker" 
+      "onepassword" 
+      "polkituser"
+      "onepassword-cli"
+    ];
+    packages = with pkgs; [
+    ];
+    shell = pkgs.zsh;
+  };
+
+
+}
