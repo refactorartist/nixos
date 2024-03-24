e9a218b2e728471fc14624bf0357246d021ddab6
Khalid Zubair <khalid.zubair@gig.com>
2024-03-24 03:04:26 +0100

    Add 1Password integration and enable polkit_gnome

---
+++ system/one_password.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }: 
+
+{
+  environment.systemPackages = with pkgs; [
+    polkit_gnome
+  ];
+
+  security.polkit.enable = true;
+
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [ "khalid" ];
+  };      
+}
