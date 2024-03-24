2daa2f7614ccb2acc780656a4e93f81c7be82949
Khalid Zubair <khalid.zubair@gig.com>
2024-03-24 05:21:34 +0100

    Add installation script for home-manager

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env sh
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager
+nix-channel --update
+
+nix-shell '<home-manager>' -A install
