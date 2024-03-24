bbdb88df3ccd0a4ac5316681f0e2270c09f1704c
Khalid Zubair <khalid.zubair@gig.com>
2024-03-24 05:21:15 +0100

    Update home-manager.url in flake.nix to use release-23.11 branch

--- flake.nix
+++ flake.nix
@@ -3,22 +3,33 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   }; 
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let 
+      system = "x86_64-linux";
       lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       nixosConfigurations = {
         nixos = lib.nixosSystem {
-          system = "x86_64-linux";
+          inherit system;
           modules = [
             ./configuration.nix
-            ./system/one_password.nix
-            ./system/users.nix
             ./system/flake_settings.nix
+            ./system/users.nix
+            ./system/one_password.nix
+            ./system/docker.nix
+          ];
+        };
+      };
+      homeConfigurations = {
+        khalid = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./home.nix
           ];
         };
       };
